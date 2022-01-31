ac2a4cb4395c4ed72423314f8ab806abde2768ee
Caitlin Flanagan <caiceili@email.unc.edu>
2022-01-31 16:20:44 -0500

    ex02_one_shot_wordle_submitted

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -3,32 +3,32 @@
 __author__ = "730406845"
 
 secret_word: str = "python"
-entered_word: str = input(f"What is your {len(secret_word)} guess? ")
+entered_word: str = input(f"What is your {len(secret_word)}-letter guess? ")
 emoji: str = ""
-guessed_character: bool = False
-i: int = 0
+idx: int = 0
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 
 while len(entered_word) != len(secret_word):
     print(f"That was not {len(secret_word)} letters! ")
-    entered_word: str = input("Try again: ")
+    entered_word = input("Try again: ")
 
-while i < len(secret_word):
-    if entered_word[i] == secret_word[i]:
+while idx < len(secret_word):
+    if entered_word[idx] == secret_word[idx]:
         emoji += GREEN_BOX
     else:
-        j: int = 0
-        while not guessed_character and j < len(secret_word):
-            if entered_word[i] == secret_word[j]:
+        guessed_character: bool = False
+        alt_idx: int = 0
+        while not guessed_character and alt_idx < len(secret_word):
+            if entered_word[idx] == secret_word[alt_idx]:
                 guessed_character = True
-            j += 1
+            alt_idx += 1
         if guessed_character:
             emoji += YELLOW_BOX
         else:
             emoji += WHITE_BOX 
-    i += 1
+    idx += 1
 
 print(emoji)
 
